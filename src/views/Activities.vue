413334d301585a65339b58c7ab1c899ffff3a9cd
Carlota Catot <carkbra@gmail.com>
2020-12-17 17:10:34 +0100

    updated activities

--- src/views/Activities.vue
+++ src/views/Activities.vue
@@ -5,66 +5,55 @@
         <h1 class="activity-title">TALKS</h1>
       </div>
       <div class="panel">
-        <h1 style="text-transform: uppercase;">TBD - Talk motivadora</h1>
+        <h1 style="text-transform: uppercase;">Refining the diagnosis</h1>
         <div class="margin-bottom text-centered">
           <p class="icon"><i class="fab fa-twitch" aria-hidden="true"></i></p>
           <span><a target="_blank" href="https://www.twitch.tv/hackersupc">Twitch Channel</a></span>
         </div>
         <div>
-          <h3>Speaker: Clara Prats</h3>
+          <h3>Speaker: Toni Soriano</h3>
           <p style="text-align: justify;">
-            Description TBD
+            Children with COVID-19 have clinical manifestations that are often difficult to differentiate from those of other viral infections that affect the respiratory system, such as the flu. Through clinical data collected by more than 120 pediatricians in Catalonia we are seeking to identify which symptoms can predict that a child has COVID and which of them have not been connected to this disease. With your collaboration, we will be able to save many unnecessary tests on those boys and girls and to generate an algorithm or clinical score that allows us to identify properly each of them. We can make it! We are convinced that you will help paediatricians in Catalonia and around the world to work better in the future.
           </p>
-          <!-- <div style="text-align: center;">
-            <img src="/assets/img/live/cloud.png" class="image" style="width: 30%; margin-top: 1em" />
-          </div> -->
         </div>
       </div>
-      <!-- <div class="panel">
-        <h1 style="text-transform: uppercase;">TBD - Test mining</h1>
+      <div class="panel">
+        <h1 style="text-transform: uppercase;">File processing and machine learning for audio classification using Python</h1>
         <div class="margin-bottom text-centered">
           <p class="icon"><i class="fab fa-twitch" aria-hidden="true"></i></p>
           <span><a target="_blank" href="https://www.twitch.tv/hackersupc">Twitch Channel</a></span>
         </div>
         <div>
-          <h3>Speaker: Martin Krallinger</h3>
-          <p style="text-align: justify;">Description TBD</p>
+          <h3>Speaker: Guillem Bonilla i Daniel Marín</h3>
+          <p style="text-align: justify;">In this talk we will show and discuss tools and libraries to load and process audio files in Python. Once the files are loaded, we will show how to build machine learning models and how to evaluate the performance of the trained model using internal and external validation. In particular, we will illustrate the complete process using cough and non-cough audio files, import metadata to the model, and show the performance of the trained model when evaluated with new data. Finally, we we will show how to build a chatbot in Telegram that can be used to obtain information from the user and include this knowledge to the model.</p>
         </div>
-      </div> -->
+      </div>
       <div class="panel">
-        <h1 style="text-transform: uppercase;">TBD </h1>
+        <h1 style="text-transform: uppercase;">Language technologies and AI applied to health and medical data</h1>
         <div class="margin-bottom text-centered">
           <p class="icon"><i class="fab fa-twitch" aria-hidden="true"></i></p>
           <span><a target="_blank" href="https://www.twitch.tv/hackersupc">Twitch Channel</a></span>
         </div>
         <div>
-          <h3>Speaker: Jordi</h3>
-          <p style="text-align: justify;">Description TBD</p>
-          <!-- <div style="text-align: center;">
-             <img src="/assets/img/live/deno.png" class="image" style="width: 50%; margin-top: 1.4em"/>
-          </div> -->
+          <h3>Speaker: Salvador Lima</h3>
+          <p style="text-align: justify;">When processing text, it is often not enough to simply extract the events found in the text. There is meaning hidden in plain view in the sentence, acting as modifiers of the information found. Recognising what events are negated or uncertain can radically change everything, and in clinical texts it can be critical for decision making and diferential diagnosis. <br /> <br /> Also, the presenter will point you to a youtube channel with a few more talks that you can look at and ask for any doubt you have.</p>
         </div>
       </div>
       <div class="panel">
-        <h1 style="text-transform: uppercase;">TBD - Interficies y privacitat</h1>
+        <h1 style="text-transform: uppercase;">Applications of Digital Contact Tracing</h1>
         <div class="margin-bottom text-centered">
           <p class="icon"><i class="fab fa-twitch" aria-hidden="true"></i></p>
           <span><a target="_blank" href="https://www.twitch.tv/hackersupc">Twitch Channel</a></span>
         </div>
         <div>
           <h3>Speaker: Jorge Garcia</h3>
-          <p style="text-align: justify;">
-            Description TBD
-          </p>
-          <!-- <div style="text-align: center;">
-            <img src="/assets/img/live/neural-ntw.png" class="image" style="width: 30%; margin-top: 2em"/>
-          </div> -->
+          <p style="text-align: justify;">The talk will be about the usefulness of DCT applications to fight the COVID-19 pandemic, which are the technologies used and aspects you should take care about privacy, security and distance measures. We will also talk about protocols DP3T/GAEN, ROBERT, IDPT, DESIRE, and which is the role of Google and Apple. Finally, we will talk about how to improve the effectiveness of these systems.</p>
         </div>
       </div>
       <div class="panel large">
         <h1 class="activity-title">ACTIVITIES</h1>
       </div>
-      <div class="panel">
+      <div class="panel large">
         <h1>TEAM BUILDING</h1>
         <div>
           <div class="margin-bottom text-centered">
@@ -77,7 +66,7 @@
           <p class="margin-bottom"> Depending on your answers, we will look for some colleges with whom you can develop a project for the challenge and also, how to create with your team a private channel. </p>
         </div>
       </div>
-      <div class="panel">
+      <div class="panel large">
         <h1>TBD - Tematica UCIs</h1>
         <div>
           <div class="margin-bottom text-centered">
@@ -85,10 +74,10 @@
             <span>ucis-activity</span>
           </div>
           <h3>Speaker: Esther i Ariadna</h3>
-          <p class="margin-bottom" style="text-align: justify;"> TBD - Description </p>
-          <!-- <div style="text-align: center;">
-            <img src="/assets/img/live/zumba.png" class="image" style="width: 100%; margin-top: 3em"/>
-          </div> -->
+          <p class="margin-bottom" style="text-align: justify;"> Would you like to see through the eyes of a health professional working in the ICU or a patient about to be intubated due to COVID-19? Do you want to know how they feel and what they think? “COVID-19 and empathy: being in their shoes” is an activity that will allow you to get to know some of the situations that ICU nurses experience in their everyday lives and which some patients have to face. <br /><br />
+The aim of the activity is to promote empathy towards those whom COVID-19 touches personally in order to be able to see the world of COVID-19 from another perspective and to be able to develop technological tools focused on those people.<br /><br />
+The activity consists of viewing two videos, which are very short and are recorded in real hospital settings. One of the videos will allow you to see through the eyes of a nurse and the other one through those of a patient. Once the visualisation is over, you will need to fill in a short questionnaire that aims to encourage reflection and to make us question the ability to connect with the other and its impact.<br /><br />
+Are you ready to go into an ICU? If so, go ahead. </p>
         </div>
       </div>
        <div class="panel large">
