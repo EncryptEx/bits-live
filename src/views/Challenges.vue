a99418bc17b9191a19d8987db9941441bad481cd
Carlota Catot <carkbra@gmail.com>
2020-12-18 22:24:50 +0100

    more info in challenges

--- src/views/Challenges.vue
+++ src/views/Challenges.vue
@@ -9,10 +9,13 @@
         </div>
         <div>
           <h3><a href="https://doi.org/10.5281/zenodo.4314709" target="_blank" class="external-link">Input data</a></h3>
-          <!-- <div class="embed-container">
-          </div> -->
+          <h3><a href="data/AIDoctor_info.pdf" target="_blank" class="external-link">More information!</a></h3>
+          <h3><a href="https://youtube.com/playlist?list=PL5uSCzf1azhBeVCHyswazImBNpIW8gYTD" target="_blank" class="external-link">YouTube Channel with more information</a></h3>
+          <div class="embed-container">
+            <iframe src="https://www.youtube.com/embed/XmMq8xEMuLQ" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
         </div>
-        <div class="expl-container">
+        <!-- <div class="expl-container">
           <div class="margin-bottom text-centered">
             <a href="https://www.bsc.es/" h-reflang="en" target="_blank" rel="sponsored">
               <img style="width: 25%;" loading="lazy" alt="BSC" src="assets/img/challenges/logo-bsc.png">
@@ -21,7 +24,7 @@
               <img style="width: 12%;" loading="lazy" alt="planTL" src="assets/img/challenges/logo_plantl.png">
             </a>
           </div>
-        </div>
+        </div> -->
       </div>
       <div class="panel">
         <h1>Clinical-microbiological characterisation of SARS-CoV-2 infection in the paediatric age</h1>
@@ -32,11 +35,11 @@
         <div>
           <h3><a href="data/COPEDICATClinicSympt_DATA_2020-12-17_1642.csv" target="_blank" class="external-link">Input data</a></h3>
           <h3><a href="data/COPEDI-CAT_clinic_symptoms_REDCap.pdf" target="_blank" class="external-link">Data dictionary</a></h3>
-          <!-- <div class="embed-container">
-            <iframe src="https://www.youtube.com/embed/YcDEur1IxCs" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div> -->
+          <div class="embed-container">
+            <iframe src="https://www.youtube.com/embed/rY_rAyjO6Nw" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
         </div>
-        <div class="expl-container">
+        <!-- <div class="expl-container">
           <div class="margin-bottom text-centered">
             <a>
               <img style="width: 25%;" loading="lazy" alt="Copedicat" src="assets/img/challenges/logo-copedicat.png">
@@ -48,7 +51,7 @@
               <img style="width: 20%;" loading="lazy" alt="bio" src="assets/img/challenges/logo-biocomsc.jpg">
             </a>
           </div>
-        </div>
+        </div> -->
       </div>
       <div class="panel">
         <h1>Do you have COVID-19 cough? Help distinguishing which type of cough you have</h1>
@@ -58,11 +61,12 @@
         </div>
         <div>
           <h3><a href="https://b2sdata.upc.edu:5001/d/f/590044504787040823" target="_blank" class="external-link">Input data</a></h3>
-          <!-- <div class="embed-container">
-            <iframe src="https://www.youtube.com/embed/YcDEur1IxCs" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div> -->
+          <h3><a href="https://github.com/b2slab/covidscipy2020/tree/mainbranch/HACKATHON%202020" target="_blank" class="external-link">Useful codes</a></h3>
+          <div class="embed-container">
+            <iframe src="https://www.youtube.com/embed/InlBAJF8Gso" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
         </div>
-        <div class="expl-container">
+        <!-- <div class="expl-container">
           <div class="margin-bottom text-centered">
             <a href="https://creb.upc.edu/en" h-reflang="en" target="_blank" rel="sponsored">
               <img style="width: 25%;" loading="lazy" alt="creb" src="assets/img/challenges/logo-creb.png">
@@ -74,7 +78,7 @@
               <img style="width: 40%;" loading="lazy" alt="upc" src="assets/img/challenges/logo-upc.png">
             </a>
           </div>
-        </div>
+        </div> -->
       </div>
       <div class="panel">
         <h1>COVIDTracking-Primary Health Care Centres: Help the front line of medical care!</h1>
@@ -84,11 +88,11 @@
         </div>
         <div>
           <h3>No input data</h3>
-          <!-- <div class="embed-container">
-            <iframe src="https://www.youtube.com/embed/YcDEur1IxCs" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div> -->
+          <div class="embed-container">
+            <iframe src="https://www.youtube.com/embed/sj_Lyo-ayQU" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
         </div>
-        <div class="expl-container">
+        <!-- <div class="expl-container">
           <div class="margin-bottom text-centered">
             <a href="https://atencioprimariaicsbcn.wordpress.com/capbesos/" h-reflang="en" target="_blank" rel="sponsored">
               <img style="width: 8%;" loading="lazy" alt="creb" src="assets/img/challenges/logo-capbesos.png">
@@ -100,7 +104,7 @@
               <img style="width: 30%;" loading="lazy" alt="upc" src="assets/img/challenges/logo-hackersupc.png">
             </a>
           </div>
-        </div>
+        </div> -->
       </div>
       <div class="panel">
         <h1>Covid-Tracking on Campus!</h1>
@@ -110,17 +114,17 @@
         </div>
         <div>
           <h3>No input data</h3>
-          <!-- <div class="embed-container">
-            <iframe src="https://www.youtube.com/embed/YcDEur1IxCs" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div> -->
+          <div class="embed-container">
+            <iframe src="https://www.youtube.com/embed/FQzpNFU6TNs" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
         </div>
-        <div class="expl-container">
+        <!-- <div class="expl-container">
           <div class="margin-bottom text-centered">
             <a style="marginLeft: 20px" href="https://www.fib.upc.edu/" h-reflang="en" target="_blank" rel="sponsored">
               <img style="width: 30%;" loading="lazy" alt="b2slab" src="assets/img/challenges/logo-FIB.jpg">
             </a>
           </div>
-        </div>
+        </div> -->
       </div>
       <div class="panel">
         <h1>Donatiu: Diu molt de tu!</h1>
@@ -129,11 +133,11 @@
             <b><span><a href="https://www.fib.upc.edu/la-marato/donatiu.html" target="_blank" class="external-link">Donate</a></span></b>
           </div>
           <h3>No input data</h3>
-          <!-- <div class="embed-container">
-            <iframe src="https://www.youtube.com/embed/YcDEur1IxCs" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div> -->
+          <div class="embed-container">
+            <iframe src="https://www.youtube.com/embed/g9RpYlUZ_AU" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
         </div>
-        <div class="expl-container">
+        <!-- <div class="expl-container">
           <div class="margin-bottom text-centered">
             <a style="marginLeft: 20px" href="https://www.fib.upc.edu/" h-reflang="en" target="_blank" rel="sponsored">
               <img style="width: 20%;" loading="lazy" alt="b2slab" src="assets/img/challenges/logo-FIB.jpg">
@@ -148,7 +152,7 @@
               <img style="width: 20%;" loading="lazy" alt="upc" src="assets/img/challenges/logo-fibalumni.png">
             </a>
           </div>
-        </div>
+        </div> -->
       </div>
       <div class="panel large">
         <h1><a href="https://www.fib.upc.edu/en/la-marato" target="_blank" class="external-link">All challenge descriptions</a></h1>
@@ -156,7 +160,7 @@
       <div class="panel large">
         <h1>Acces to Marenostrum+Power9 and GPU</h1>
         <p>
-          <b><a href="assets/data/MachineInformation_only.pdf" target="_blank" class="external-link">Basic information</a></b> <br/>
+          <b><a href="https://www.fib.upc.edu/sites/fib/files/bitsxlamarato-user-reponsabilities-bsc-2020.pdf" target="_blank" class="external-link">Basic information</a></b> <br/>
           If you want access contact a organizer in the <span>#help</span> channel on discord.
         </p>
       </div>
