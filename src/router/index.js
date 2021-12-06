c25bdf8df9f529e072603bb9929f27b20c7b1298
carlotacb <carkbra@gmail.com>
2021-12-06 15:13:26 +0100

    deleted unnecesary commponents for this edition

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,9 @@ import Home from '@/views/Home.vue';
 import FAQ from '@/views/FAQ.vue';
 import Activities from '@/views/Activities.vue';
 import Challenges from '@/views/Challenges.vue';
-import Donations from '@/views/Donations.vue';
-import Discord from '@/views/Discord.vue';
 import Schedule from '@/views/Schedule.vue';
 import Live from '@/views/Live.vue';
 import Rules from '@/views/Rules.vue';
-import Judging from '@/views/Judging.vue';
 import FullScreen from '@/views/FullScreen.vue';
 
 Vue.use(VueRouter);
@@ -49,21 +46,6 @@ const routes = [
     path: '/fullscreen',
     name: 'FullScreen',
     component: FullScreen,
-  },
-  {
-    path: '/donations',
-    name: 'Donations',
-    component: Donations,
-  },
-  {
-    path: '/discord',
-    name: 'Discord',
-    component: Discord,
-  },
-  {
-    path: '/judging',
-    name: 'Judging',
-    component: Judging,
   },
   {
     path: '/rules',
